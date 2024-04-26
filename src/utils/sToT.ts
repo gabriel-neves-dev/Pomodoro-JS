6796615c05c15a2f8787da6c9e7cabdf247eb4c0
Gabriel Neves <gabrielneves.dev@gmail.com>
2024-04-26 17:58:58 -0300

    added seconds to time

---
+++ src/utils/sToT.ts
@@ -0,0 +1,7 @@
+export function secondsToTime(seconds: number): string {
+    const zeroLeft = (n: number) => Math.floor(n);
+    
+    const min = zeroLeft((seconds / 60) % 60).toString().padStart(2, '0');
+    const sec = zeroLeft((seconds % 60) % 60).toString().padStart(2, '0');
+    return `${min}:${sec}`
+}
