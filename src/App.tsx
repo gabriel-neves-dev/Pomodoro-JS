629c6d705011af4abb251cca43e06d9c03e32a7b
Gabriel Neves <gabrielneves.dev@gmail.com>
2024-05-07 14:23:50 -0300

    fixed pomodoro timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
 import {PomodoroTimer} from './components/pomodoro-timer';
 
 
@@ -9,7 +9,12 @@ function App() {
   return (
     <div className="App"> 
       
-      <PomodoroTimer pomodoroTime={1500} shortRestTime={300} longRestTime={900} cycles={4} />
+      <PomodoroTimer 
+      pomodoroTime={1500} 
+      shortRestTime={300} 
+      longRestTime={900} 
+      cycles={4} 
+      />
       
     </div>
   );
